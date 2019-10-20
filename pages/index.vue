75f32e6b2b0e3c91d0ac925bf3e5c388612b0fbb
Morten Sørensen <morten@moso.io>
2019-10-20 23:34:57 +0200

    Updated index for new layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,32 @@
 <template>
-    <div></div>
+    <profile />
 </template>
 
 <script>
-export default {
+import Profile from '~/pages/profile.vue'
 
+export default {
+    layout: 'default',
+
+    components: {
+        'profile': Profile
+    },
+
+    data() {
+        return {
+            title: 'Profile'
+        }
+    },
+
+    head() {
+        return {
+            title: this.title + ' - moso.io'
+        }
+    },
+
+    mounted() {
+        this.$store.commit('SET_PAGE_TITLE', this.title)
+    }
 }
 </script>
 
