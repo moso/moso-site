7439dda4c1c129d574292259746894fe37b0d2f1
Morten Sørensen <morten@moso.io>
2019-10-20 23:36:16 +0200

    Updated default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        <nuxt />
+    <div id="app">
+        <moso-header />
+        <main>
+            <nuxt />
+        </main>
+        <moso-footer />
     </div>
 </template>
+
+<script>
+import mosoHeader from '~/components/header'
+import mosoFooter from '~/components/footer'
+
+export default {
+    components: {
+        'moso-header': mosoHeader,
+        'moso-footer': mosoFooter
+    }
+}
+</script>
