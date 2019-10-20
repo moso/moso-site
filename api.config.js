fa0ddc501adc8884ddfb08687ffed06d0eb64016
Morten Sørensen <morten@moso.io>
2019-10-20 23:34:33 +0200

    Added old api endpoints

---
+++ api.config.js
@@ -0,0 +1,20 @@
+// .env values for Netlify
+export const profileApi = {
+    titles: process.env.titles,
+    devicons: process.env.devicons,
+    skills: process.env.skills,
+    profiles: process.env.profiles,
+    infolists: process.env.infolists,
+    tools: process.env.tools
+}
+
+export const projectsApi = {
+    projects: process.env.projects,
+    smallprojects: process.env.smallprojects
+
+}
+
+export const resumeApi = {
+    experiences: process.env.experiences,
+    educations: process.env.educations
+}
