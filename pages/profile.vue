dd91c69b33aeb15a217e1bf9c6e6c3a70fb47b82
Morten Sørensen <morten@moso.io>
2019-10-21 16:33:42 +0200

    Updated profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,28 +1,45 @@
 <template>
-    <div></div>
+    <article>
+        <div v-for="title in titles" :key="title.id">
+            <profile image="/images/profile.jpg" name="Morten Sørensen" :position="title.title">
+                <profile-item v-for="icon in icons" :key="icon.id" :link="icon.url" :link-class="icon.name.toLowerCase().trim()" :svg="icon.svg" :title="icon.name" />
+            </profile>
+        </div>
+
+        <h1 class="hidden">{{ pageTitle }}</h1>
+        <!-- eslint-disable-next-line vue/no-v-html -->
+        <div v-for="bio in bios" :key="bio.id" v-html="bio.text"></div>
+    </article>
 </template>
 
 <script>
+import axios from 'axios'
+
+import Profile from '~/components/profile/profile.vue'
+import ProfileItem from '~/components/profile/profile-item.vue'
+
 import { profileApi } from '~/api.config'
 
 export default {
     layout: 'default',
 
     components: {
-        //
+        'profile': Profile,
+        'profile-item': ProfileItem
     },
 
     data() {
         return {
-            title: 'Profile',
+            pageTitle: 'Profile',
             titles: [],
-            tools: []
+            icons: [],
+            bios: []
         }
     },
 
     head() {
         return {
-            title: this.title + ' - moso.io'
+            title: this.pageTitle + ' - moso.io'
         }
     },
 
@@ -32,27 +49,52 @@ export default {
         }
 
         const promises = []
-        /* eslint-disable-next-line no-unused-vars */
+        // eslint-disable-next-line no-unused-vars
         for (const [key, value] of Object.entries(profileApi)) {
-            promises.push($axios.$get(`${value}`))
+            promises.push(axios.get(`${value}`))
         }
 
         const [
             { data: titles },
-            { data: tools }
+            { data: icons },
+            { data: bios }
         ] = await Promise.all(promises)
-        return {
-            titles,
-            tools
-        }
-        /* eslint-disable-next-line no-unreachable */
+
         if(process.client) {
             window.performance.mark('getProfile:end')
+        }
+
+        return {
+            titles,
+            icons,
+            bios
         }
     },
 
     mounted() {
-        this.$store.commit('SET_PAGE_TITLE', this.title)
+        this.$store.commit('SET_PAGE_TITLE', this.pageTitle)
     }
 }
 </script>
+
+<style lang="scss" scoped>
+article {
+    display: flex;
+    flex-direction: column;
+
+    .hidden {
+        display: none;
+    }
+}
+</style>
+
+<style lang="scss">
+article
+    > div:last-of-type {
+        p {
+            &:last-of-type {
+                margin: 1rem 0 0;
+            }
+        }
+    }
+</style>
