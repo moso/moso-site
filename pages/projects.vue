138e68cb219d3f24925aafd2952129437a568b93
Morten Sørensen <morten@moso.io>
2019-10-21 16:33:57 +0200

    Updated projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,8 +1,25 @@
 <template>
-    <div></div>
+    <article>
+        <h1>{{ pageTitle }}</h1>
+
+        <h2>Open-source</h2>
+        <ul class="list">
+            <li v-for="project in projects" :key="project.id">
+                <a :href="project.url" target="_blank" rel="noopener" :title="project.title">{{ project.title }}</a>
+                <p>{{ project.subtitle }}</p>
+            </li>
+        </ul>
+
+        <h2>I've also made things for...</h2>
+        <ul class="list">
+            <li v-for="project in smallprojects" :key="project.id">{{ project.title }}</li>
+        </ul>
+    </article>
 </template>
 
 <script>
+import axios from 'axios'
+
 import { projectsApi } from '~/api.config'
 
 export default {
@@ -14,7 +31,7 @@ export default {
 
     data() {
         return {
-            title: 'Projects',
+            pageTitle: 'Projects',
             projects: [],
             smallprojects: []
         }
@@ -22,11 +39,11 @@ export default {
 
     head() {
         return {
-            title: this.title + ' - moso.io'
+            title: this.pageTitle + ' - moso.io'
         }
     },
 
-    async asyncData({ $axios }) {
+    async asyncData({ req, params }) {
         if(process.client) {
             window.performance.mark('getProjects:start')
         }
@@ -34,25 +51,50 @@ export default {
         const promises = []
         /* eslint-disable-next-line no-unused-vars */
         for (const [key, value] of Object.entries(projectsApi)) {
-            promises.push($axios.$get(`${value}`))
+            promises.push(axios.get(`${value}`))
         }
 
         const [
             { data: projects },
             { data: smallprojects }
         ] = await Promise.all(promises)
+
+        if(process.client) {
+            window.performance.mark('getProjects:end')
+        }
+
         return {
             projects,
             smallprojects
         }
-        /* eslint-disable-next-line no-unreachable */
-        if(process.client) {
-            window.performance.mark('getProfile:end')
-        }
     },
 
     mounted() {
-        this.$store.commit('SET_PAGE_TITLE', this.title)
+        this.$store.commit('SET_PAGE_TITLE', this.pageTitle)
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.list {
+    padding: 0;
+    list-style: none;
+
+    li {
+        margin: .625rem 0 0;
+
+        p {
+            margin: 0;
+            font-size: .9125rem;
+        }
+    }
+}
+
+article {
+    > .list {
+        &:first-of-type {
+            margin: 0 0 3rem;
+        }
+    }
+}
+</style>
