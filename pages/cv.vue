164aad9d078dcb1d53f1c060991396a1adfc28f3
Morten Sørensen <morten@moso.io>
2019-10-21 16:33:10 +0200

    Updated CV page

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,8 +1,21 @@
 <template>
-    <div></div>
+    <article>
+        <h1>{{ pageTitle }}</h1>
+        <h2>Experience</h2>
+        <ul class="list">
+            <li v-for="exp in reverseExp" :key="exp.id"> {{ exp.period }} <strong>{{ exp.title }}</strong> &#64;{{ exp.location }}</li>
+        </ul>
+
+        <h2>Education</h2>
+        <ul class="list">
+            <li v-for="edu in reverseEdu" :key="edu.id"> {{ edu.year }} <strong>{{ edu.title }}</strong></li>
+        </ul>
+    </article>
 </template>
 
 <script>
+import axios from 'axios'
+
 import { cvApi } from '~/api.config'
 
 export default {
@@ -14,7 +27,7 @@ export default {
 
     data() {
         return {
-            title: 'CV',
+            pageTitle: 'CV',
             experiences: [],
             educations: []
         }
@@ -22,37 +35,64 @@ export default {
 
     head() {
         return {
-            title: this.title + ' - moso.io'
+            title: this.pageTitle + ' - moso.io'
         }
     },
 
-    async asyncData({ $axios }) {
+    computed: {
+        reverseExp() {
+            return this.experiences.slice().reverse();
+        },
+        reverseEdu() {
+            return this.educations.slice().reverse();
+        }
+    },
+
+    async asyncData({ req, params }) {
         if(process.client) {
             window.performance.mark('getCv:start')
         }
 
         const promises = []
-        /* eslint-disable-next-line no-unused-vars */
+        // eslint-disable-next-line no-unused-vars
         for (const [key, value] of Object.entries(cvApi)) {
-            promises.push($axios.$get(`${value}`))
+            promises.push(axios.get(`${value}`))
         }
 
         const [
             { data: experiences },
             { data: educations }
         ] = await Promise.all(promises)
+
+        if(process.client) {
+            window.performance.mark('getCv:end')
+        }
+
         return {
             experiences,
             educations
         }
-        /* eslint-disable-next-line no-unreachable */
-        if(process.client) {
-            window.performance.mark('getCv:end')
-        }
     },
 
     mounted() {
-        this.$store.commit('SET_PAGE_TITLE', this.title)
+        this.$store.commit('SET_PAGE_TITLE', this.pageTitle)
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.list {
+    padding: 0;
+    list-style: none;
+
+    li {
+        margin: .625rem 0 0;
+    }
+}
+
+article > .list {
+    &:first-of-type {
+        margin: 0 0 2rem;
+    }
+}
+</style>
