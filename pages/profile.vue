6661936e0ce0122cd23c560dd6ba2b8d31eaf7e9
Morten Sørensen <morten@moso.io>
2019-10-22 10:28:43 +0200

    Removed

--- pages/profile.vue
+++ pages/profile.vue
@@ -43,7 +43,7 @@ export default {
         }
     },
 
-    async asyncData({ $axios }) {
+    async asyncData({ req, params }) {
         if(process.client) {
             window.performance.mark('getProfile:start')
         }
