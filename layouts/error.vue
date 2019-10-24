10af2de59cf81372db983ce0b03e6aeda00f59d9
Morten Sørensen <morten@moso.io>
2019-10-24 13:29:30 +0200

    Added wizard emoji

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
     <article>
         <h1>{{ pageTitle }}</h1>
-        <h2>Not all those who wander are lost</h2>
+        <h2>🧙🏻 Not all those who wander are lost</h2>
         <p>You totally are, though...</p>
         <p>Renewed shall be the page that was broken, if you go <nuxt-link to="/">back home</nuxt-link>.</p>
     </article>
