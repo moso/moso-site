bf21109d89bd3536d782817295ab92f0b47b46b7
Morten Sørensen <morten@moso.io>
2019-11-15 09:39:28 +0100

    Added Vuex store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,86 @@
 export const state = () => ({
-    pageTitle: 'Default Title'
+    pageTitle: 'Default Title',
+    titles: [],
+    bios: [],
+    experiences: [],
+    educations: [],
+    tools: [],
+    projects: [],
+    things: [],
+    icons: []
 })
 
 export const mutations = {
-    SET_PAGE_TITLE (state, title) {
+    SET_PAGE_TITLE(state, title) {
         state.pageTitle = title
+    },
+
+    setTitles(state, titles) {
+        state.titles = titles
+    },
+
+    setBios(state, bios) {
+        state.bios = bios
+    },
+
+    setExperiences(state, experiences) {
+        state.experiences = experiences
+    },
+
+    setEducations(state, educations) {
+        state.educations = educations
+    },
+
+    setTools(state, tools) {
+        state.tools = tools
+    },
+
+    setProjects(state, projects) {
+        state.projects = projects
+    },
+
+    setThings(state, things) {
+        state.things = things
+    },
+
+    setIcons(state, icons) {
+        state.icons = icons
+    }
+}
+
+export const actions = {
+    async getTitles({ commit }) {
+        const titles = await this.$axios.$get(`${process.env.titles}`)
+        commit('setTitles', titles)
+    },
+
+    async getBios({ commit }) {
+        const bios = await this.$axios.$get(`${process.env.bios}`)
+        commit('setBios', bios)
+    },
+
+    async getExperiences({ commit }) {
+        const experiences = await this.$axios.$get(`${process.env.experiences}`)
+        commit('setExperiences', experiences)
+    },
+
+    async getEducations({ commit }) {
+        const educations = await this.$axios.$get(`${process.env.educations}`)
+        commit('setEducations', educations)
+    },
+
+    async getProjects({ commit }) {
+        const projects = await this.$axios.$get(`${process.env.projects}`)
+        commit('setProjects', projects)
+    },
+
+    async getThings({ commit }) {
+        const things = await this.$axios.$get(`${process.env.things}`)
+        commit('setThings', things)
+    },
+
+    async getIcons({ commit }) {
+        const icons = await this.$axios.$get(`${process.env.icons}`)
+        commit('setIcons', icons)
     }
 }
